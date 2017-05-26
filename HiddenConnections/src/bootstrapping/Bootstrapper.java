bab7447b5f9f52ad220df9903bf0f96d6fc2e57d
VeraCLHD <vera.boteva@yahoo.de>
2017-05-26 18:43:46 +0100

    created abstract class for botstrapping

---
+++ HiddenConnections/src/bootstrapping/Bootstrapper.java
@@ -0,0 +1,10 @@
+package bootstrapping;
+
+public abstract class Bootstrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HiddenConnections/src/bootstrapping/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A package for the implementation of the bootstrapping algorithm.
+ */
+/**
+ * @author XMobile
+ *
+ */
+package bootstrapping;
