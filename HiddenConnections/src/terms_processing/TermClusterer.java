897aa23d96a76700025b1f638c0460faa0b46a86
Vera Boteva <Boteva@stud.uni-heidelberg.de>
2017-07-03 20:45:06 +0200

    finished clustering of diseases (without suppl concepts), removed additional terms

--- HiddenConnections/src/terms_processing/TermClusterer.java
+++ HiddenConnections/src/terms_processing/TermClusterer.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import bootstrapping.Bootstrapper;
@@ -24,29 +25,96 @@ import edu.mit.jwi.item.POS;
 import edu.mit.jwi.morph.WordnetStemmer;
 import io.Reader;
 import io.Writer;
+import terms_processing.StanfordLemmatizer;
 
 public class TermClusterer {
+	private static final String MESH_VARIANTS_ENRICHED_TXT = "mesh/meshVariants_enriched.txt";
+	private static Map<String, String> categorizedTerms = new HashMap<String, String>();
+	// key: meshId, value: list of all entry terms for id + term itself
+	private static Map<String, Set<String>> meshTerms =  new HashMap<String, Set<String>>();
+	// key: meshId, value: list of all tree structures
+	private static Map<String, Set<String>> meshTrees =  new HashMap<String, Set<String>>();
 	
+	public static final String pathToAllTermsAfterWordNet = "terms/all_terms_and_variants_with10_filtered_clustered.txt";
+	// this variable is for adding the lemma to the variants
+	private static StanfordLemmatizer lemmatizer = new StanfordLemmatizer();
 	
-
-	private static Map<String, String> termWordNet = new HashMap<String, String>();
-	public static final String pathToAllTermsAfterWordNet = "terms/all_terms_and_variants_with10_filtered_WN.txt";
+	private static final String MESH_DESC2017_ASCII = "mesh/d2017.bin";
+	private static final String MESH_MESHTREES_ASCII = "mesh/mtrees2017.bin";
+	// this file is actually secondary
+	private static final String MESH_QUAL2017_ASCII = "mesh/q2017.bin";
+	private static final String MESH_SUPP2017_ASCII = "mesh/c2017.bin";
 	
 	public static void main(String[] args) throws IOException{
+		String[] files = {MESH_DESC2017_ASCII};
 		Writer.overwriteFile("", pathToAllTermsAfterWordNet);
         IDictionary dict = prepareExtraction();
         TermClusterer tc = new TermClusterer();
         readAllTerms();
-        for(String t: termWordNet.keySet()){
+        // is only needed once
+        createEnrichedMeshDescFile();
+        //createEnrichedMeshSuppFile();
+        useMeshEntries();
+        for(String t: categorizedTerms.keySet()){
         	 tc.findLexNameForTerm(dict, t);
+        	 tc.categorizeTermWithMesh(t);
+        	 tc.finalCategorizationFoods(t);
 		}
         
         writeAllTerms();
        
         
     }
-
-	private void findLexNameForTerm(IDictionary dict, String term) {
+	
+	public static void useMeshEntries(){
+		List<String> lines = Reader.readLinesList(MESH_VARIANTS_ENRICHED_TXT);
+		for(String line: lines){
+			if(!line.matches("\\s+") && !line.isEmpty()){
+				Set<String> tree_set = new HashSet<String>();
+				Set<String> variants_set = new HashSet<String>();
+				
+				String[] lineArray = line.split("\t");
+				String term = lineArray[0];
+				String id = lineArray[1];
+				String trees = lineArray[2];
+				String[] variants_a = {};
+				
+				if(lineArray.length>3){
+					String variants = lineArray[3];
+					variants_a = variants.split(",");
+				}
+				
+				
+				String[] trees_a = trees.split(",");
+				
+				
+				for(String tree_entry: trees_a){
+					if(!tree_entry.equals("")){
+						tree_set.add(tree_entry.trim());	
+					}
+				}
+				
+				for(String variant: variants_a){
+					if(!variants_set.equals("")){
+						variants_set.add(variant.trim());
+					}
+						
+				}
+				
+				variants_set.add(term);
+				
+			TermClusterer.getMeshTerms().put(id, variants_set);
+			TermClusterer.getMeshTrees().put(id, tree_set);
+				
+			}
+		}
+	}
+	/**
+	 * This method takes care of finding the lexicographer's file name: noun.food, noun.plant
+	 * @param dict
+	 * @param term
+	 */
+	public void findLexNameForTerm(IDictionary dict, String term) {
 		 WordnetStemmer stem =  new WordnetStemmer(dict);
          Set<String> termAndLemma = new HashSet<String>();
          List<String> stems = stem.findStems(term, POS.NOUN);
@@ -65,7 +133,7 @@ public class TermClusterer {
                 
                 if(LexFileName.equals("noun.food") || LexFileName.equals("noun.plant") || term.contains("food")){
                 	
-                		TermClusterer.getTermWordNet().put(term, "FOOD");
+                		TermClusterer.getcategorizedTerms().put(term, "FOOD");
                 }
             } 
         	 
@@ -73,22 +141,83 @@ public class TermClusterer {
 
 	}
 	
-	public static void readAllTerms(){
-		List<String> lines = Reader.readLinesList(Bootstrapper.pathToAllTerms);
-		for(String line: lines){
-			if(!line.isEmpty() && !line.equals(" ")){
-				String term = line.trim().toLowerCase();
-				TermClusterer.termWordNet.put(term.trim(), "INTERMEDIARY_CONCEPT");
-			}
-		}
+	public void categorizeTermWithMesh(String term){
+		if(TermClusterer.getcategorizedTerms().get(term).equals("INTERMEDIARY_CONCEPT")){
+			Set<Entry<String, Set<String>>> set = TermClusterer.getMeshTerms().entrySet();
+			for(Entry<String, Set<String>> entry: set){
+				if(entry.getValue().contains(term)){
+					String id = entry.getKey();
+					Set<String> mesh_tree = TermClusterer.getMeshTrees().get(id);
+					for(String tree_entry: mesh_tree){
+						// C: all diseases, F03.: psychological disorders, B04: viruses
+						if(tree_entry.startsWith("C") ||tree_entry.startsWith("F03.") || tree_entry.startsWith("B04.")){
+							TermClusterer.getcategorizedTerms().put(term, "DISEASE");
+							// B01.650: plants, J02 foods
+						} else if(tree_entry.startsWith("B01.650") || tree_entry.startsWith("J02.")){
+							TermClusterer.getcategorizedTerms().put(term, "FOOD");
+						} else if(tree_entry.equals("-")){
+							// supplementary concept files
+							if(TermClusterer.isDisease(term)){
+								TermClusterer.getcategorizedTerms().put(term, "DISEASE");
+							}
+						}
+					}
+					
+				} else{
+					if(TermClusterer.isDisease(term)){
+						TermClusterer.getcategorizedTerms().put(term, "DISEASE");
+					}
+				}
+			}	
+		}	
 	}
 	
-	public static void writeAllTerms(){
-		for(String t: termWordNet.keySet()){
-			Writer.appendLineToFile(t + "\t" + termWordNet.get(t), pathToAllTermsAfterWordNet);
-		}
+	public void finalCategorizationFoods(String term){
+		if(term.contains(" ")){
+			String[] termArray = term.split(" ");
+			String lastWord = termArray[termArray.length-1];
+			
+			if(TermClusterer.getcategorizedTerms().get(term).equals("INTERMEDIARY_CONCEPT")){
+				Set<Entry<String, Set<String>>> set = TermClusterer.getMeshTerms().entrySet();
+				for(Entry<String, Set<String>> entry: set){
+					if(entry.getValue().contains(lastWord)){
+						String id = entry.getKey();
+						Set<String> mesh_tree = TermClusterer.getMeshTrees().get(id);
+						for(String tree_entry: mesh_tree){
+							if(tree_entry.startsWith("B01.650") || tree_entry.startsWith("J02.")){
+								TermClusterer.getcategorizedTerms().put(term, "FOOD");
+							} 
+						}
+			
+					}
+					}
+				}
+			}
 	}
-
+	public static boolean isDisease(String term_variant){
+		boolean result = false;
+		String[] candidate_disease = {
+				"disease", "diseases", "disorder", "disorders",
+				"deficiency", "deficiencies", 
+				"injury", "injuries", 
+				"condition", "conditions",
+				"cancer", "cancers",
+				"dysfunction", "dysfunction"};
+		for(String criterium: candidate_disease){
+			if(term_variant.endsWith(criterium)){
+				result = true;
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Prepares WordNet extraction.
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	private static IDictionary prepareExtraction() throws MalformedURLException, IOException {
 		//construct URL to WordNet Dictionary directory on the computer
         String wordNetDirectory = "WordNet 2.1";
@@ -101,8 +230,167 @@ public class TermClusterer {
 		return dict;
 	}
 	
-	public static Map<String, String> getTermWordNet() {
-		return termWordNet;
+	public static void readAllTerms(){
+		List<String> lines = Reader.readLinesList(Bootstrapper.pathToAllTerms);
+		for(String line: lines){
+			if(!line.isEmpty() && !line.equals(" ")){
+				String term = line.trim().toLowerCase();
+				// the first step is to declare everything as intermediary concept.
+				TermClusterer.categorizedTerms.put(term.trim(), "INTERMEDIARY_CONCEPT");
+			}
+		}
+	}
+	
+	public static void writeAllTerms(){
+		for(String t: categorizedTerms.keySet()){
+			Writer.appendLineToFile(t + "\t" + categorizedTerms.get(t), pathToAllTermsAfterWordNet);
+		}
+	}
+
+	public static void createEnrichedMeshDescFile(){
+		
+		Writer.overwriteFile("", MESH_VARIANTS_ENRICHED_TXT);
+		// here: read the ready MeshVariants file -> write also the tree number (MN) and search for id
+		Set<String> tree_number = new HashSet<String>();
+		String name = "";
+		String id = "";
+		Set<String> meshVariants = new HashSet<String>();
+		
+
+			List<String> lines = Reader.readLinesList(MESH_DESC2017_ASCII);
+			for(String line: lines){
+				if(line.equals("*NEWRECORD")){
+					String newLine = "";
+					if(name.contains(", ")){
+						String[] arr = name.split(",");
+						String first_word = arr[1].trim();
+						String second_word = arr[0].trim();
+						name = first_word + " " + second_word;
+					}
+					newLine += name +"\t";
+					newLine += id +  "\t";
+					
+					for(String tn: tree_number){
+						newLine += tn + ",";
+					}
+					newLine += "\t";
+					
+					for(String mv: meshVariants){
+						newLine += mv + ",";
+					}
+					
+					
+					Writer.appendLineToFile(newLine, MESH_VARIANTS_ENRICHED_TXT);
+					tree_number = new HashSet<String>();
+					name = "";
+					id = "";
+					meshVariants = new HashSet<String>();
+				} else if(line.startsWith("MH =")){
+					String[] array = line.split("=");
+					name = array[1].trim().toLowerCase();
+				} else if(line.startsWith("MN =")){
+					String[] array = line.split("=");
+					tree_number.add(array[1].trim());
+				} else if(line.startsWith("UI")){
+					String[] array = line.split("=");
+					id = array[1].trim();
+				} else if(line.startsWith("ENTRY") || line.startsWith("PRINT ENTRY")){
+					String[] array = line.split("=");
+					String entry = array[1].trim().toLowerCase();
+					String meshVariant = "";
+					if(entry.contains("|")){
+						meshVariant = entry.substring(0, entry.indexOf("|")).toLowerCase();
+					} else{
+						meshVariant = entry;
+					}
+					
+					
+					if(meshVariant.contains(", ")){
+						String[] arr = meshVariant.split(",");
+						String first_word = arr[1].trim();
+						String second_word = arr[0].trim();
+						meshVariant = first_word + " " + second_word;
+					}
+					
+					meshVariants.add(meshVariant);
+				}
+			}
+		}
+
+public static void createEnrichedMeshSuppFile(){
+		// no tree number in supplementary files
+		String tree_number = "-";
+		String name = "";
+		String id = "";
+		Set<String> meshVariants = new HashSet<String>();
+		
+
+			List<String> lines = Reader.readLinesList(MESH_SUPP2017_ASCII);
+			for(String line: lines){
+				if(line.equals("*NEWRECORD")){
+					String newLine = "";
+					if(name.contains(", ")){
+						String[] arr = name.split(",");
+						String first_word = arr[1].trim();
+						String second_word = arr[0].trim();
+						name = first_word + " " + second_word;
+					}
+					newLine += name +"\t";
+					newLine += id +  "\t";
+					newLine += tree_number + ",";
+					
+					newLine += "\t";
+					
+					for(String mv: meshVariants){
+						newLine += mv + ",";
+					}
+					
+					
+					Writer.appendLineToFile(newLine, MESH_VARIANTS_ENRICHED_TXT);
+					name = "";
+					id = "";
+					meshVariants = new HashSet<String>();
+				} else if(line.startsWith("NM =")){
+					String[] array = line.split("=");
+					name = array[1].trim().toLowerCase();
+				} else if(line.startsWith("SY") || line.startsWith("HM")){
+					String[] array = line.split("=");
+					String entry = array[1].trim().toLowerCase().replaceAll("\\*", "");
+					String meshVariant = "";
+					if(entry.contains("|")){
+						meshVariant = entry.substring(0, entry.indexOf("|")).toLowerCase();
+					} else{
+						meshVariant = entry;
+					}
+					
+					
+					if(meshVariant.contains(", ")){
+						String[] arr = meshVariant.split(",");
+						String first_word = arr[1].trim();
+						String second_word = arr[0].trim();
+						meshVariant = first_word + " " + second_word;
+					}
+					
+					meshVariants.add(meshVariant);
+				} 
+				else if(line.startsWith("UI")){
+					String[] array = line.split("=");
+					id = array[1].trim();
+				} 
+			}
+		}
+
+	
+	public static Map<String, String> getcategorizedTerms() {
+		return categorizedTerms;
+	}
+
+	public static Map<String, Set<String>> getMeshTerms() {
+		return meshTerms;
+	}
+
+	public static Map<String, Set<String>> getMeshTrees() {
+		return meshTrees;
 	}
 
 }
---
+++ HiddenConnections/src/terms_processing/XMLParserMesh.java
@@ -0,0 +1,102 @@
+package terms_processing;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import javax.lang.model.util.Elements;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import io.Writer;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class XMLParserMesh {
+
+private static final String MESH_QUAL2017_XML = "mesh/qual2017.xml";
+private static final String MESH_DESC2017_XML = "mesh/desc2017.xml";
+private StanfordLemmatizer lemmatizer = new StanfordLemmatizer();
+
+public static void main(String argv[]) {
+	Writer.overwriteFile("", "meshIDs.txt");
+    extractEntryTermsMeshHeadings(MESH_DESC2017_XML, "DescriptorRecord", "DescriptorUI", "DescriptorName");
+    //extractEntryTermsMeshHeadings("mesh/supp2017.xml", "SupplementalRecord", "SupplementalRecordUI", "SupplementalRecordName");
+    extractEntryTermsMeshHeadings(MESH_QUAL2017_XML, "QualifierRecord", "QualifierUI", "QualifierName");
+    
+  }
+
+public static void extractEntryTermsMeshHeadings(String path, String rootTag, String idTag, String nameTag) {
+	try {
+
+	File fXmlFile = new File(path);
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+	
+	NodeList nList = doc.getElementsByTagName(rootTag);
+	System.out.println(doc.getTextContent());
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+		Node nNode = nList.item(temp);
+		String node = "";
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			
+			Element eElement = (Element) nNode;
+			String name = eElement.getElementsByTagName(nameTag).item(0).getTextContent().toLowerCase();
+			
+			System.out.println(name);
+			
+			if(name.contains(",") && name.contains(" ")){
+				String[] arr = name.split(",");
+				String first = arr[1].trim();
+				String second = arr[0].trim();
+				name = first + " " + second;
+			} 
+			node += name + "\t";
+			node += eElement.getElementsByTagName(idTag).item(0).getTextContent().toLowerCase() + "\t";
+			
+			NodeList nl = eElement.getElementsByTagName("Term");
+			for(int i = 0; i<nl.getLength(); i++){
+				Node termNode = nl.item(i);
+				Element term = (Element) termNode;
+				String meshVariant = term.getElementsByTagName("String").item(0).getTextContent();
+				
+				if( meshVariant != null && !meshVariant.equals("") && !meshVariant.isEmpty()){
+					
+					
+					if(meshVariant.contains(",") && meshVariant.contains(" ")){
+						String[] arr = meshVariant.split(",");
+						String first_word = arr[1].trim();
+						String second_word = arr[0].trim();
+						meshVariant = first_word + " " + second_word;
+					}
+					
+					node += meshVariant.toLowerCase() + ",";
+				} 
+				
+				
+
+			}
+			
+			
+
+		}
+		
+		Writer.appendLineToFile(node, "meshVariants.txt");
+		
+	}
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+}
+
+}
